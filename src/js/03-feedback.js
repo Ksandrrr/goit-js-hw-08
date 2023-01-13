94d5c59dff82e079d197e50a1baf6469fe2bfae3
Ksandrrr <105207003+Ksandrrr@users.noreply.github.com>
2023-01-13 17:14:14 +0200

    correct feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,59 +1,6 @@
 
-// const form = document.querySelector(`.feedback-form`)
-// const input = document.querySelector(`input`)
-// const textarea = document.querySelector(`textarea`)
-// const button = document.querySelector(`button`)
-
-
-////////1
-
-// let localStorageObj;;
-// let currentTextStorage;
-// let formTextContObj;
-// let parc;
-// currentTextarea()
-// form.addEventListener(`input`, function form(e) {
-//    const {
-//     elements: { email, message }
-//    } = e.currentTarget;
-//   formTextContObj = {email: email.value, message: message.value}
-//   currentStorageJSON  = JSON.stringify(formTextContObj)
-//   let local = localStorage.setItem("feedback-form-state", currentStorageJSON)
-//   return local
-// })
-
-// function currentTextarea() {
-//  const savedSettings = localStorage.getItem("feedback-form-state");
-//   parc = currentTextStorage = JSON.parse(savedSettings);
-//   if (savedSettings) {
-//     textarea.value = parc.message
-//   }
-//   if (savedSettings) {
-//     input.value = parc.email
-//   }
-// }
-
-// button.addEventListener(`submit`, submitButton)
-// function submitButton(e) {
-// console.log(`click`)
-
-// }
-
-// function textArea() {
-//  textarea.value = currentTextStorage.message
-// }
-
-
-// input.addEventListener(`input`, inputANDtextarea)
-// textarea.addEventListener(`input`, textArea)
-
-  
-
-
-/////////////2
-
-
 var throttle = require('lodash.throttle');
+
 const refs = {
   form: document.querySelector(`.feedback-form`),
   input: document.querySelector(`input`),
@@ -64,41 +11,54 @@ refs.form.addEventListener(`submit`, throttle(formSubm,500))
 refs.textarea.addEventListener(`input`, throttle(TeaxareaSave, 500))
 refs.input.addEventListener(`input`, throttle(inputEmail,500))
 
-currentTextarea()
+
+
+let messageStorage = {};
+let saveMessage
+let messageObj;
+
 function formSubm(e) {
-  e.preventDefault()
-  localStorage.clear()
-  let consoleMessage = { name: message, email: messageInput};
+    e.preventDefault()
+    console.log(messageStorage)
+    e.currentTarget.reset()
+    localStorage.clear()
+    messageStorage = {}
 
-  if (consoleMessage.name === `` || consoleMessage.email === ``) {
-    return 
-  }else {
-    e.target.reset()
-    console.log(consoleMessage)
-  }
 }
-
-let message = ``;
-let messageInput = ``;
 function TeaxareaSave(evt) {
+  localStorage.setItem("feedback-form-state", JSON.stringify(messageStorage))
+ saveMessage = localStorage.getItem("feedback-form-state")
+ messageObj = JSON.parse(saveMessage)
+  messageStorage.message = evt.target.value
+  if (evt.target.value === ``) {
+    evt.target.value = ``
+  }
 
-  message = evt.target.value
-  localStorage.setItem("feedback-form-state", message)
-}
-function currentTextarea() {
-  
-  const saveMessage = localStorage.getItem("feedback-form-state")
-  if (saveMessage) {
-    refs.textarea.value = saveMessage
-  }
-  const saveEmail = localStorage.getItem("feedback-form-input")
-  if (saveEmail) {
-    refs.input.value = saveEmail
-  }
 }
 function inputEmail(evt) {
-
-  messageInput = evt.target.value
-  localStorage.setItem("feedback-form-input", messageInput)
+  localStorage.setItem("feedback-form-state", JSON.stringify(messageStorage))
+  saveMessage = localStorage.getItem("feedback-form-state")
+  messageObj = JSON.parse(saveMessage)
+  messageStorage.email = evt.target.value
+if (evt.target.value === ``) {
+    evt.target.value = ``
+  }
 }
+currentTextarea()
+function currentTextarea() {
+ const saveMesValue = localStorage.getItem("feedback-form-state")
+ const parsMesValue = JSON.parse(saveMesValue)
+  if (parsMesValue) {
+    refs.input.value = parsMesValue.email
+    messageStorage.email = parsMesValue.email
+  } 
+  
+
+  if (parsMesValue) {
+    refs.textarea.value = parsMesValue.message
+    messageStorage.message = parsMesValue.message
+  }
+ 
+}
+
 
